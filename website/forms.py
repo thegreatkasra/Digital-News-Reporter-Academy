4b743acb8da4953c4c1e0b7c0ea0648e0af3a9bf
kasra torabi <Kasra.torabiii@gmail.com>
2023-09-11 10:22:31 +0430

    copy static and templates!
    sakhte base va load statics

---
+++ website/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from website.models import Contact , Newsletter
+from captcha.fields import CaptchaField
+
+class NameForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    subject = forms.CharField(max_length=50)
+    massage = forms.CharField(widget=forms.Textarea)
+    
+class ContactForm(forms.ModelForm):
+    subject = forms.CharField(required=False)
+    captcha = CaptchaField()
+    class Meta:
+        model = Contact
+        fields = ['name', 'subject','email', 'message']
+
+        def clean_name(self):
+            return 'unknown'
+           
+    
+class NewsletterForm(forms.ModelForm):
+    class Meta:
+        model = Newsletter
+        fields = ['email']
